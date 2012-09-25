9f9967c4c87308799f5629f24a46e4fa31e4e44f
Thom Ritterfeld <thom@ritterfeld.com>
2012-09-25 17:55:08 +0200

    changed gitrepotypo:

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com/tritter/blogstore.git"
+set :repository, "git@github.com:tritter/blogstore.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
